a49fdd818f2ad1bf21423a1fb1a46805e32f2c28
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-19 14:27:27 +0900

    パスの設定

--- src/router.js
+++ src/router.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Skill from './views/Skill.vue'
+import Portfolio from './views/Portfolio.vue'
+import Contact from './views/Contact.vue'
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   routes: [
-    { path: "/", component: Home }
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/skill", component: Skill },
+    { path: "/portfolio", component: Portfolio },
+    { path: "/contact", component: Contact }
   ]
 });
