bd6de9c30b9b5fbcdc7e196df3a340d4e8abbd7b
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-23 14:33:16 +0900

    エラーの修正

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Skill from './views/Skill.vue'
-import Portfolio from './views/Portfolio.vue'
-import Contact from './views/Contact.vue'
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Skill from './views/Skill.vue';
+import Portfolio from './views/Portfolio.vue';
+import Contact from './views/Contact.vue';
 
 Vue.use(Router);
 
